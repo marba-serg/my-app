a148268f71ffa9c651e269f56021091138d30e9f
marba-serg <marba@ukr.net>
2022-06-16 11:30:20 +0300

    add: add props to CostItem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import CostItem from './components/CostItem'
 
 function App() {
+  
+  const сosts = [
+    {
+      date: new Date(2021, 2, 12),
+      description: 'Холодильник',
+      amount: 999.99
+    },
+    {
+      date: new Date(2022, 5, 12),
+      description: 'Телевизор',
+      amount: 499.99
+    },
+    {
+      date: new Date(2022, 5, 12),
+      description: 'Ковер',
+      amount: 99.99
+    }
+  ]
+  
   return (
     <div>
       <h1>Приложение на REACT</h1>
       <p>You know I feel that this cource is really greate</p>
-      <CostItem/>
-      <CostItem/>
-      
+      <CostItem date = {сosts[0].date} description = {сosts[0].description} amount = {сosts[0].amount}></CostItem>
+      <CostItem date = {сosts[1].date} description = {сosts[1].description} amount = {сosts[1].amount}></CostItem>
+      <CostItem date = {сosts[2].date} description = {сosts[2].description} amount = {сosts[2].amount}></CostItem>
+    
     </div>
   );
 }
--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,20 +1,22 @@
 import './CostItem.css'
 
 
-function CostItem (){
-
-const costDate = new Date(2021, 2, 12)
-const costDescription = 'Холодильник'
-const costAmount = 999.99
-
+function CostItem (props){
+const month = props.date.toLocaleString('ru-RU', {month: "long"})
+const year = props.date.getFullYear()
+const day = props.date.toLocaleString('ru-RU', {day: "2-digit"})
     return (
     <div className = 'cost-item'>
-        <div>{costDate.toISOString()}</div>
-        <div className = 'cost-item__description'>
-          <h2>{costDescription}</h2>
-          <div className = 'cost-item__price'>${costAmount}</div>
+        <div>
+          <div>{month}</div>
+          <div>{year}</div>
+          <div>{day}</div>
         </div>
 
+        <div className = 'cost-item__description'>
+          <h2>{props.description}</h2>
+          <div className = 'cost-item__price'>${props.amount}</div>
+        </div>
     </div>
     );
  
