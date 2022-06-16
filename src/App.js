2015128df8e9b540e912716b78b5029bca77e5c8
marba-serg <marba@ukr.net>
2022-06-16 10:01:34 +0300

    add: add react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import CostItem from './components/CostItem'
+
+function App() {
+  return (
+    <div>
+      <h1>Приложение на REACT</h1>
+      <p>You know I feel that this cource is really greate</p>
+      <CostItem/>
+      <CostItem/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CostItem.js
@@ -0,0 +1,23 @@
+import './CostItem.css'
+
+
+function CostItem (){
+
+const costDate = new Date(2021, 2, 12)
+const costDescription = 'Холодильник'
+const costAmount = 999.99
+
+    return (
+    <div className = 'cost-item'>
+        <div>{costDate.toISOString()}</div>
+        <div className = 'cost-item__description'>
+          <h2>{costDescription}</h2>
+          <div className = 'cost-item__price'>${costAmount}</div>
+        </div>
+
+    </div>
+    );
+ 
+}
+
+export default CostItem
